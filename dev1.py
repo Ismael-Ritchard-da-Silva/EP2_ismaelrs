8f029168596fea68a399e9f46910099eb338d474
Ismael Silva <ismaelrs@al.insper.edu.br>
2022-10-26 08:15:13 -0300

    Função transforma base acrescentada, agrupa questões por nível

--- dev1.py
+++ dev1.py
@@ -1,1 +1,15 @@
 print('Ismael R. Silva')
+
+def transforma_base(l):
+    dic = {}
+
+    for i in range(len(l)):
+        for j, k in l[i].items():
+            if j == 'nivel':
+                if k not in dic.keys():
+                    dic[k] = []
+                    dic[k].append(l[i])
+                elif k in dic.keys():
+                    dic[k].append(l[i])
+    
+    return dic
